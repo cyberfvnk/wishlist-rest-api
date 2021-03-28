ce4c7ea9e7b769bb049d92858b263e8c638fa44d
Cyberfunker <monicafs94@gmail.com>
2021-03-28 02:00:16 -0300

    add comentários

--- flask_api.py
+++ flask_api.py
@@ -1,15 +1,16 @@
+import os
+import psycopg2
+import requests
 from flask import Flask, redirect, url_for, render_template, request, session
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql.expression import func
-import psycopg2, os, requests
-
 
 DATABASE_URL = os.environ['DATABASE_URL']
-DATABASE_USER = os.environ['DATABASE_USER']
+USER_PASSWORD = os.environ['USER_PASSWORD']
 APP_SECRET_KEY = os.environ['APP_SECRET_KEY']
 BASE_URL = os.environ['BASE_URL']
- 
+
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 app = Flask(__name__)
@@ -32,78 +33,92 @@ class Wishlist(db.Model):
 
 # argumentos do item
 item_put_args = reqparse.RequestParser()
-item_put_args.add_argument("desc", type=str)  
+item_put_args.add_argument("desc", type=str)
 item_put_args.add_argument("link", type=str)
-item_put_args.add_argument("pic", type=str) 
-item_put_args.add_argument("possui", type=bool) 
+item_put_args.add_argument("pic", type=str)
+item_put_args.add_argument("possui", type=bool)
 
-# argumentos do item a serem editados na lista
+# argumentos do item a serem editados
 item_update_args = reqparse.RequestParser()
-item_update_args.add_argument("desc", type=str)  
-item_update_args.add_argument("link", type=str)  
+item_update_args.add_argument("desc", type=str)
+item_update_args.add_argument("link", type=str)
 item_update_args.add_argument("pic", type=str)
 
-
+# parâmetros para o filtro do decorator marshal_with
 resource_fields = {"item": fields.String,
                    "desc": fields.String,
                    "link": fields.String,
                    "pic": fields.String,
-                   "possui":fields.Boolean}
+                   "possui": fields.Boolean}
 
 
 class Item(Resource):
 
     @marshal_with(resource_fields)
-    def get(self, item):
-        output = Wishlist.query.filter_by(item=item).first()
+    def get(self, item): 
+        # retorna o item passado como parâmetro
+        wishlist_item = Wishlist.query.filter_by(item=item).first()
 
-        if not output:
-            abort(404, message="O item não foi encontrado.")
+        if not wishlist_item:
+            # checa se o item existe
+            abort(404, message="Item not found.")
 
-        return output, 200
+        return wishlist_item, 200
+
 
     @marshal_with(resource_fields)
-    def put(self, item):
+    def put(self, item): 
+        # insere na lista o item passado como parâmetro, aceitando parâmetros opcionais
         args = item_put_args.parse_args()
-        output = Wishlist.query.filter_by(item=item).first()
+        wishlist_item = Wishlist.query.filter_by(item=item).first()
 
-        if output:
+        if wishlist_item: 
+            # checa se o item já existe na lista
             abort(409, message="O item inserido já existe.")
 
+        # cria o objeto item
         item = Wishlist(item=item, desc=args["desc"], link=args["link"], pic=args["pic"], possui=False)
-        
+
+        # adiciona o item na db
         db.session.add(item)
         db.session.commit()
-        
+
         return item, 201
 
-    @marshal_with(resource_fields)
-    def patch(self,item):
-        args = item_update_args.parse_args()
-        output = Wishlist.query.filter_by(item=item).first()
 
-        if not output:
+    @marshal_with(resource_fields)
+    def patch(self, item):
+        # edita as características do item passado como parâmetro
+        args = item_update_args.parse_args()
+        wishlist_item = Wishlist.query.filter_by(item=item).first()
+
+        if not wishlist_item:
+            # checa se o item existe
             abort(404, message="Item não encontrado.")
 
+        # atualiza os argumentos caso existam alterações
         if args["desc"]:
-            output.desc = args["desc"]
+            wishlist_item.desc = args["desc"]
         if args["link"]:
-            output.link = args["link"]
+            wishlist_item.link = args["link"]
         if args["pic"]:
-            output.pic = args["pic"]
+            wishlist_item.pic = args["pic"]
 
         db.session.commit()
 
-        return output
+        return wishlist_item, 200
+
 
     @marshal_with(resource_fields)
     def delete(self, item):
-        output = Wishlist.query.filter_by(item=item).first()
+        # deleta o item passado como parâmetro
+        wishlist_item = Wishlist.query.filter_by(item=item).first()
 
-        if not output:
+        if not wishlist_item:
+            # checa se o item existe
             abort(404, message="Item não encontrado.")
-            
-        db.session.delete(output)        
+
+        db.session.delete(wishlist_item)
         db.session.commit()
 
         return "", 204
@@ -111,101 +126,121 @@ class Item(Resource):
 
 class Owned(Resource):
 
+    def get(self, item):
+        # redireciona para o patch
+        response = requests.patch(BASE_URL + r"wishlist/" + item + r"/owned/")
+
     @marshal_with(resource_fields)
-    def get(self,item):
-        response = requests.patch(BASE_URL + "wishlist/" + item + "/owned/")
+    def patch(self, item):
+        # altera o status do item para obtido/não obtido
+        wishlist_item = Wishlist.query.filter_by(item=item).first()
 
-
-    @marshal_with(resource_fields)
-    def patch(self,item):
-        output = Wishlist.query.filter_by(item=item).first()
-
-        if not output:
+        if not wishlist_item:
+            # checa se o item existe
             abort(404, message="Item não encontrado.")
 
-        output.possui = not output.possui
+        wishlist_item.possui = not wishlist_item.possui
         db.session.commit()
 
 
-class Random(Resource): 
+class Random(Resource):
 
-    @marshal_with(resource_fields) 
+    @marshal_with(resource_fields)
     def get(self):
-        output = Wishlist.query.filter_by(possui=False).order_by(func.random()).first()
+        # retorna um item aleatório da lista (considera apenas os itens não obtidos) 
+        wishlist_item = Wishlist.query.filter_by(possui=False).order_by(func.random()).first()
 
-        if not output:
+        if not wishlist_item:
+            # checa se há itens disponíveis na lista
             abort(404, message="Não há itens disponíveis na lista.")
 
-        return output, 200
-
+        return wishlist_item, 200
 
 
 @app.route("/")
 def home():
+    # template para a tela inicial
     return render_template("index.html")
 
-@app.route("/login", methods =["POST", "GET"])
+
+@app.route("/login", methods=["POST", "GET"])
 def login():
+    # realiza o login
     if request.method == "POST":
+        # recebe o password
         password = request.form["pwd"]
 
-        if password in DATABASE_USER:
+        # checa se o password está na lista de senhas
+        if password in USER_PASSWORD:
             user = "Admin"
 
+        # adiciona o user a sessão
         session["user"] = user
+
+        # redireciona para a wishlist
         return redirect(url_for("wishlist"))
+
     else:
         if "user" in session:
+            # caso já tenha uma sessão iniciada, redireciona para a wishlist
             return redirect(url_for("wishlist"))
-        return render_template("login.html")
+           
+        return render_template("login.html") # exibe a página de login 
+
 
 @app.route("/logout")
 def logout():
-    session.pop("user", None)
-    return redirect(url_for("home"))
+    session.pop("user", None) # realiza o logout retirando o user da sessão
+    return redirect(url_for("home")) # redireciona a página inicial
 
-@app.route("/wishlist", methods =["POST", "GET", "PATCH"])
+
+@app.route("/wishlist", methods=["POST", "GET"])
 def wishlist():
-
     if "user" in session:
+        #checa se há sessão iniciada
         user = session["user"]
 
         if request.method == "POST":
+
+            # recebe os parâmetros
             item = request.form["item"]
             desc = request.form["desc"]
             link = request.form["link"]
             pic = request.form["pic"]
-            
+
+            # cria o item
             data = {item: {"desc": desc,
-                            "link": link, 
-                            "pic": pic}}
+                           "link": link,
+                           "pic": pic}}
 
+            # recebe a ação selecionada
             action = request.form["btnradio"]
-            
+
+            # realiza o método de acordo com a ação
             if action == "edit":
-                response = requests.patch(BASE_URL + "wishlist/" + item, data[item])
-                # print(BASE_URL + "wishlist/" + item, data[item])     
+                response = requests.patch(BASE_URL + r"wishlist/" + item, data[item])
             elif action == "delete":
-                response = requests.delete(BASE_URL + "wishlist/" + item)
+                response = requests.delete(BASE_URL + r"wishlist/" + item)
             elif action == "status":
-                response = requests.patch(BASE_URL + "wishlist/" + item + "/owned/")
+                response = requests.patch(BASE_URL + r"wishlist/" + item + r"/owned/")
             else:
-                response = requests.put(BASE_URL + "wishlist/" + item, data[item])
-   
+                response = requests.put(BASE_URL + r"wishlist/" + item, data[item])
+
+        # retorna o template da wishlist, enviando os objetos necessários
         return render_template("wishlist.html",
-                            lista_all=enumerate(Wishlist.query.filter_by(possui=False).all()), 
-                            lista_owned=enumerate(Wishlist.query.filter_by(possui=True).all()), 
-                            URL_want=["/wishlist/" + i.item for i in Wishlist.query.filter_by(possui=False).all()], 
-                            URL_own=["/wishlist/" + i.item for i in Wishlist.query.filter_by(possui=True).all()])
+                               lista_all=enumerate(Wishlist.query.filter_by(possui=False).all()),
+                               lista_owned=enumerate(Wishlist.query.filter_by(possui=True).all()),
+                               URL_want=[r"/wishlist/" + i.item for i in Wishlist.query.filter_by(possui=False).all()],
+                               URL_own=[r"/wishlist/" + i.item for i in Wishlist.query.filter_by(possui=True).all()])
     else:
-        return redirect(url_for("login"))
+        return redirect(url_for("login")) # redireciona para login caso não haja sessão
 
-
-
+#rotas da api
 api.add_resource(Item, r"/wishlist/<string:item>")
 api.add_resource(Owned, r"/wishlist/<string:item>/owned/")
 api.add_resource(Random, r"/wishlist/random/")
 
+# roda o app
 if __name__ == "__main__":
     db.create_all()
-    app.run(debug=True)
+    app.run()
